c6e096457beaa339f3e025da33f50d0e2e8c9f57
Gustavo Soldera <133945929+g-soldera@users.noreply.github.com>
2024-11-25 20:18:40 -0300

    convert into record

--- src/main/java/brcomkassin/blockLimiter/limiter/PlacedBlock.java
+++ src/main/java/brcomkassin/blockLimiter/limiter/PlacedBlock.java
@@ -5,15 +5,4 @@ import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class PlacedBlock {
-    private final UUID playerUuid;
-    private final String groupId;
-    private final Material material;
-    private final Location location;
-    private final long placedAt;
-} 
+public record PlacedBlock(UUID playerUuid, String groupId, Material material, Location location, long placedAt) {} 
